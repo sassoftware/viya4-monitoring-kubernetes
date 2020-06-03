9f06bb5725a85ac4f13a711c27e051b081ed7918
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2020-06-03 15:32:09 -0400

    Logging: (re-)add Copyright and License notes to deploy_logging_open.sh

--- logging/bin/deploy_logging_open.sh
+++ logging/bin/deploy_logging_open.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Copyright © 2020, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 source logging/bin/common.sh
 
 # Elasticsearch user customizations
