402513cff4042a24a5cc26eb02e0ada8998f7612
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2020-06-14 20:48:30 -0400

    Logging: Remove use of annotations for Crunchy Data, etcd; new parsers

--- logging/bin/deploy_logging_fluentbit_open.sh
+++ logging/bin/deploy_logging_fluentbit_open.sh
@@ -6,7 +6,6 @@
 source logging/bin/common.sh
 
 HELM_DEBUG="${HELM_DEBUG:-false}"
-NGINX_NS="${NGINX_NS:-ingress-nginx}"
 
 # Fluent Bit user customizations
 FB_OPEN_USER_YAML="${FB_OPEN_USER_YAML:-logging/user-values-fluent-bit-open.yaml}"
@@ -19,22 +18,6 @@ kubectl -n $LOG_NS apply -f logging/fb/fluent-bit_config.configmap_open.yaml
 # Create ConfigMap containing Viya-customized parsers (delete it first)
 kubectl -n $LOG_NS delete configmap fb-viya-parsers --ignore-not-found
 kubectl -n $LOG_NS create configmap fb-viya-parsers  --from-file=logging/fb/viya-parsers.conf
-
-# Annotate cluster-level pods with parser suggestions
-kubectl -n kube-system annotate pod -l "component=etcd" fluentbit.io/parser=etcd --overwrite
-
-# Handle NGINX
-set +e
-kubectl get ns $NGINX_NS &>/dev/null
-if [ $? == 0 ]; then
-  nginxFound=true
-fi
-set -e
-
-if [ "$nginxFound" == "true" ]; then
-  echo "NGINX found; will attempt to annotating pod with parser suggestion..."
-  kubectl -n $NGINX_NS  annotate pod -l "app.kubernetes.io/name=ingress-nginx" fluentbit.io/parser=nginx-ingress --overwrite
-fi
 
 # Delete any existing Fluent Bit pods in the $LOG_NS namepace (otherwise Helm chart may assume an upgrade w/o reloading updated config
 kubectl -n $LOG_NS delete pods -l "app=fluent-bit"
