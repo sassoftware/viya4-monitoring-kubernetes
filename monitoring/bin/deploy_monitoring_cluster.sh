74e0a40beb847c387ad25c7d040c196446160a2a
Bryan Ellington <Bryan.Ellington@sas.com>
2020-06-28 00:19:26 -0400

    Early self-signed TLS support using openssl

--- monitoring/bin/deploy_monitoring_cluster.sh
+++ monitoring/bin/deploy_monitoring_cluster.sh
@@ -62,18 +62,78 @@ fi
 
 # Optional TLS Support
 if [ "$MON_TLS_ENABLE" == "true" ]; then
-  cat monitoring/values-prom-operator-tls.yaml >> $genValuesFile
+  if [ ! $(which openssl) ]; then
+    echo "openssl not found on the current PATH"
+    exit 1
+  fi
+  if [ -z "$CA_KEY" ] && [ -z "$CA_CERT" ]; then
+    CA_SAVE_DIR=${CA_SAVE_DIR:-$HOME/.kube-viya-monitoring}
+    mkdir -p "$CA_SAVE_DIR"
+
+    CA_KEY=${CA_KEY:-$CA_SAVE_DIR/rootCA.key}
+    CA_CERT=${CA_CERT:-$CA_SAVE_DIR/rootCA.pem}
+
+    CA_PASS=${CA_PASS:-ops4viya}
+    CA_DAYS=${CA_DAYS:-1024}
+    CA_SUBJ=${CA_SUBJ:-/C=US/ST=North Carolina/O=SAS Institute/distinguished_name=kube-viya-monitoring}
+
+    # Look for saved CAs
+    if [ ! -f "$CA_SAVE_DIR/rootCA.key" ]; then
+      # Generate new CA key
+      openssl genrsa -des3 -out "$CA_KEY" -passout "pass:$CA_PASS" 2048
+    fi
+    if [ ! -f "$CA_SAVE_DIR/rootCA.pem" ]; then
+      # Generate new CA cert
+      openssl req -x509 -new -nodes -key "$CA_KEY" -sha256 -days "$CA_DAYS" -out "$CA_CERT" -passin "pass:$CA_PASS" -subj "$CA_SUBJ"
+    fi
+  elif [ -z "$CA_KEY" ] || [ -z "$CA_CERT" ] || [ -z $"CA_PASS" ]; then
+    log_error "Must specify CA_KEY, CA_CERT, and CA_PASS to use an exististing CA"
+    log_error "  CA_KEY =[$CA_KEY]"
+    log_error "  CA_CERT=[$CA_CERT]"
+    exit 1
+  else
+    log_info "Using existing CA"
+    log_info "  CA_KEY =[$CA_KEY]"
+    log_info "  CA_CERT=[$CA_CERT]"
+    log_info '  CA_PASS=******'
+    log_info "  CA_DAYS=[$CA_DAYS]"
+  fi
+
+  # Generate server certificates
+  KEY_FILE=${KEY_FILE:-$TMP_DIR/tls.key}
+  CERT_FILE=${CERT_FILE:-$TMP_DIR/tls.crt}
+  CSR_FILE=${CSR_FILE:-$TMP_DIR/server.csr}
+  V3_EXT_FILE=$TMP_DIR/v3.ext
+  cp monitoring/tls/v3.ext "$V3_EXT_FILE"
+  echo "DNS.1 = $MON_INGRESS_HOST" >> "$V3_EXT_FILE"
+  echo "DNS.2 = *.$MON_INGRESS_HOST" >> "$V3_EXT_FILE"
+
+  openssl req -new -sha256 -nodes -out "$CSR_FILE" -newkey rsa:2048 -keyout "$KEY_FILE" -config <( cat monitoring/tls/server.csr.cnf )
+  openssl x509 -req -in "$CSR_FILE" -CA "$CA_CERT" -CAkey "$CA_KEY" -CAcreateserial -out "$CERT_FILE" -days 500 -sha256 -extfile "$V3_EXT_FILE" -passin "pass:$CA_PASS"
+
+  # Prometheus
+  TLS_SECRET_NAME=$MON_NS-prometheus-tls-secret
+  kubectl delete --ignore-not-found secret -n $MON_NS $TLS_SECRET_NAME
+  kubectl create secret tls -n $MON_NS ${TLS_SECRET_NAME} --key ${KEY_FILE} --cert ${CERT_FILE}
+  kubectl label secret -n $MON_NS ${TLS_SECRET_NAME} sas.com/monitoring-base=kube-viya-monitoring
+  # Grafana
+  TLS_SECRET_NAME=$MON_NS-grafana-tls-secret
+  kubectl delete --ignore-not-found secret -n $MON_NS $TLS_SECRET_NAME
+  kubectl create secret tls -n $MON_NS ${TLS_SECRET_NAME} --key ${KEY_FILE} --cert ${CERT_FILE}
+  kubectl label secret -n $MON_NS ${TLS_SECRET_NAME} sas.com/monitoring-base=kube-viya-monitoring
+
+  cat monitoring/tls/values-prom-operator-tls.yaml >> $genValuesFile
 
   log_info "Provisioning TLS-enabled Prometheus datasource for Grafana..."
   cp monitoring/tls/grafana-datasource-prom-https.yaml $TMP_DIR/grafana-datasource-prom-https.yaml
   if [ "$HELM_VER_MAJOR" == "3" ]; then
     kubectl delete cm -n $MON_NS --ignore-not-found prometheus-operator-grafana-datasource
-    echo "      url: https://prometheus-operator-prometheus:9490" >> $TMP_DIR/grafana-datasource-prom-https.yaml
+    echo "      url: https://prometheus-operator-prometheus" >> $TMP_DIR/grafana-datasource-prom-https.yaml
   else
     kubectl delete cm -n $MON_NS --ignore-not-found prometheus-$MON_NS-grafana-datasource
-    echo "      url: https://prometheus-$MON_NS-prometheus:9490" >> $TMP_DIR/grafana-datasource-prom-https.yaml
+    echo "      url: https://prometheus-$MON_NS-prometheus" >> $TMP_DIR/grafana-datasource-prom-https.yaml
   fi
-  
+
   kubectl delete cm -n $MON_NS --ignore-not-found grafana-datasource-prom-https
   kubectl create cm -n $MON_NS grafana-datasource-prom-https --from-file $TMP_DIR/grafana-datasource-prom-https.yaml
   kubectl label cm -n $MON_NS grafana-datasource-prom-https grafana_datasource=1 sas.com/monitoring-base=kube-viya-monitoring
@@ -90,7 +150,7 @@ log_info "Deploying Prometheus Operator. This may take a few minutes (15min time
 log_info "User response file: [$PROM_OPER_USER_YAML]"
 if [ "$HELM_VER_MAJOR" == "3" ]; then
   log_debug "Installing via Helm 3..."
-  promRelease=prometheus-operator  
+  promRelease=prometheus-operator
   helm $helmDebug upgrade --install $promRelease \
     --namespace $MON_NS \
     -f monitoring/values-prom-operator.yaml \
