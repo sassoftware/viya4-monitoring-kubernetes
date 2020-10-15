8c79202777eb33bfd1cd2b56c9c46780afeb4cfe
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2020-10-15 15:21:43 -0400

    Removed remnants supporting demo security config

--- logging/bin/common.sh
+++ logging/bin/common.sh
@@ -18,7 +18,6 @@ if [ "$SAS_LOGGING_COMMON_SOURCED" = "" ]; then
     export LOG_NS="${LOG_NS:-logging}"
     export SAS_LOGGING_COMMON_SOURCED=true
     # TLS is required for logging components
-    export TLS_ENABLE=true
-    export TLS_CERT_MANAGER_ENABLE="${TLS_CERT_MANAGER_ENABLE:-$TLS_ENABLE}"
+    export TLS_CERT_MANAGER_ENABLE="${TLS_CERT_MANAGER_ENABLE:-true}"
 fi
 echo ""
