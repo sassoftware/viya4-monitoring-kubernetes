cfe61db91f04a8578fe9d1d5a932ef27d25cec94
Bryan Ellington <Bryan.Ellington@sas.com>
2020-10-28 15:36:34 -0400

    Parse full helm client and server versions, condition 'helm repo add --force update' on version >= 3.3.2

--- bin/helm-include.sh
+++ bin/helm-include.sh
@@ -11,17 +11,19 @@ if [ ! $(which helm) ]; then
   exit 1
 fi
 
-helmVer=$(helm version 2>/dev/null)
-if [[ "$helmVer" =~ 'Version:"v3.' ]]; then
-  HELM_VER_MAJOR=3
-  HELM_VER_FULL=$(helm version --short)
-elif [[ "$helmVer" =~ 'SemVer:"v2.' ]]; then
-  HELM_VER_MAJOR=2
-  HELM_VER_FULL=$(helm version --short --client --template '{{ .Client.SemVer }}')
-  HELM_SERVER_VER_FULL=$(helm version --short --template '{{ .Server.SemVer }}')
-else
-  echo "Error: Unable to determine helm version from [$(which helm) -version]: [$helmVer]"
-  exit 1
+helmVer=$(helm version --short 2>/dev/null)
+hver=( $(echo ${helmVer//[^0-9]/ }) )
+HELM_VER_MAJOR=${hver[0]}
+HELM_VER_MINOR=${hver[1]}
+HELM_VER_PATCH=${hver[2]}
+HELM_VER_FULL=$HELM_VER_MAJOR.$HELM_VER_MINOR.$HELM_VER_PATCH
+if [ "$HELM_VER_MAJOR" == "2" ]; then
+  helmServerVer=$(helm version --short --template '{{ .Server.SemVer }}')
+  hsver=( $(echo ${helmServerVer//[^0-9]/ }) )
+  HELM_SERVER_VER_MAJOR=${hsver[0]}
+  HELM_SERVER_VER_MINOR=${hsver[1]}
+  HELM_SERVER_VER_PATCH=${hsver[2]}
+  HELM_SERVER_VER_FULL=$HELM_SERVER_VER_MAJOR.$HELM_SERVER_VER_MINOR.$HELM_SERVER_VER_PATCH
 fi
 
 function helm2ReleaseExists {
@@ -88,7 +90,18 @@ function helmRepoAdd {
     if [ "$HELM_FORCE_REPO_UPDATE" == "true" ]; then
       log_debug "Forcing update of [$repo] helm repo to [$repoURL]"
       if [ "$HELM_VER_MAJOR" == "3" ]; then
-        helm repo add --force-update $repo $repoURL
+        # Helm 3.3.2 changed 'repo add' behavior and added the --force-update flag
+        # https://github.com/helm/helm/releases/tag/v3.3.2
+        if [ $HELM_VER_MINOR -lt 3 ]; then
+          helm repo add $repo $repoURL
+        elif [ $HELM_VER_MINOR -eq 3 ]; then
+          if [ $HELM_VER_PATCH -lt 2 ]; then
+            helm repo add $repo $repoURL
+          else
+            helm repo add --force-update $repo $repoURL
+          fi
+        else # HELM_VER_MINOR > 3
+          helm repo add --force-update $repo $repoURL
       else
         # Helm 2.x behavior is to replace by default
         helm repo add $repo $repoURL
