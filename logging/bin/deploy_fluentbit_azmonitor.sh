7b79bf20ebf118d4b331433e1e7b1fe701190ba5
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2021-02-03 12:37:08 -0500

    Added serviceMonitor notice to azmonitor FB deploy script

--- logging/bin/deploy_fluentbit_azmonitor.sh
+++ logging/bin/deploy_fluentbit_azmonitor.sh
@@ -77,10 +77,28 @@ fi
 
 # Check for an existing Helm release of stable/fluent-bit
 if helm3ReleaseExists fbaz $LOG_NS; then
-  log_info "Removing an existing release of deprecated stable/fluent-bit Helm chart from from the [$LOG_NS] namespace [$(date)]"
-  helm  $helmDebug  delete -n $LOG_NS fbaz
+   log_info "Removing an existing release of deprecated stable/fluent-bit Helm chart from from the [$LOG_NS] namespace [$(date)]"
+   helm  $helmDebug  delete -n $LOG_NS fbaz
+
+   add_notice ""
+   add_notice "================================================================================"
+   add_notice "==                                                                            =="
+   add_notice "== If you have deployed the metric monitoring components of Viya 4 Monitoring =="
+   add_notice "== for Kubernetes, you *MAY* need to deploy an updated serviceMonitor to      =="
+   add_notice "== collect metrics from Fluent Bit after this update.                         =="
+   add_notice "==                                                                            =="
+   add_notice "== Review the 'Logging - Fluent Bit' dashboard within Grafana to determine    =="
+   add_notice "== if metrics are still being collected.  If metrics are NOT being collected, =="
+   add_notice "== you should update your ServiceMonitors by running the following command:   =="
+   add_notice "==                                                                            =="
+   add_notice "==            monitoring/bin/deploy_monitoring_cluster.sh                     =="
+   add_notice "==                                                                            =="
+   add_notice "================================================================================"
+   add_notice ""
+   serviceMonitorNotice=true
 else
-  log_debug "No existing release of the deprecated stable/fluent-bit Helm chart was found"
+   log_debug "No existing release of the deprecated stable/fluent-bit Helm chart was found"
+   serviceMonitorNotice=false
 fi
 
 # Create ConfigMap containing Fluent Bit configuration
@@ -95,6 +113,15 @@ kubectl -n $LOG_NS delete pods -l "app=fluent-bit, fbout=azuremonitor"
 
 # Deploy Fluent Bit via Helm chart
 helm $helmDebug upgrade --install v4m-fbaz         --namespace $LOG_NS --values logging/fb/fluent-bit_helm_values_azmonitor.yaml --values $FB_AZMONITOR_USER_YAML  --set fullnameOverride=v4m-fbaz fluent/fluent-bit
+
+if [ "$serviceMonitorNotice" == "true" ]; then
+   LOGGING_DRIVER=${LOGGING_DRIVER:-false}
+   if [ "$LOGGING_DRIVER" != "true" ]; then
+      echo ""
+      display_notices
+      echo ""
+   fi
+fi
 
 log_info "Fluent Bit deployment (Azure Monitor) completed"
 
