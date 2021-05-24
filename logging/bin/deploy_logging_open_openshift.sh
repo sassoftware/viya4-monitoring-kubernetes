68ea3dc1bc548ab374619e919a8808439c573e77
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2021-05-24 14:32:23 -0400

    Added index policy for infrastructure (openshift-*) indexes

--- logging/bin/deploy_logging_open_openshift.sh
+++ logging/bin/deploy_logging_open_openshift.sh
@@ -55,11 +55,11 @@ logging/bin/deploy_elasticsearch_open.sh
 
 
 ##################################
-# Open Distro Content            #
+# ODFE Content (OpenShift)       #
 ##################################
 
 log_info "STEP 2a: Loading Content into Elasticsearch"
-logging/bin/deploy_elasticsearch_content_open.sh
+logging/bin/deploy_elasticsearch_content_open_openshift.sh
 
 
 ##################################
