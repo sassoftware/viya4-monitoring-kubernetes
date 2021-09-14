848748fc19200f2969b320db5a484aeba3af7232
Bryan Ellington <Bryan.Ellington@sas.com>
2021-09-14 11:58:23 -0400

    Enable user-configurable namespace deletion timeout (#171)

--- monitoring/bin/remove_monitoring_cluster.sh
+++ monitoring/bin/remove_monitoring_cluster.sh
@@ -34,7 +34,7 @@ fi
 
 if [ "$MON_DELETE_NAMESPACE_ON_REMOVE" == "true" ]; then
   log_info "Deleting the [$MON_NS] namespace..."
-  if kubectl delete namespace $MON_NS; then
+  if kubectl delete namespace $MON_NS --timeout $KUBE_NAMESPACE_DELETE_TIMEOUT; then
     log_info "[$MON_NS] namespace and monitoring components successfully removed"
     exit 0
   else
