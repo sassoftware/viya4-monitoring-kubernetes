b55aa509b38b81c3e8f99ca8591440dc18b65c74
Bryan Ellington <Bryan.Ellington@sas.com>
2022-02-17 13:16:59 -0600

    Update minimum supported Kubernetes version to 1.20 (#299)

--- logging/bin/es_nodeport_disable_open.sh
+++ logging/bin/es_nodeport_disable_open.sh
@@ -12,11 +12,4 @@ export SVC=v4m-es-client-service
 
 log_info "Removing NodePort for Elasticsearch instance [$SVC] in [$LOG_NS]"
 
-if [[ $KUBE_SERVER_VER =~ v1.18 ]]; then
-   kubectl -n "$LOG_NS" patch svc "$SVC" --type='json' -p '[{"op":"replace","path":"/spec/type","value":"ClusterIP"},{"op":"remove","path":"/spec/ports/0/nodePort"},{"op":"remove","path":"/spec/ports/1/nodePort"},{"op":"remove","path":"/spec/ports/2/nodePort"}]' 
-elif [[ $KUBE_SERVER_VER =~ v1.19 ]]; then
-   kubectl -n "$LOG_NS" patch svc "$SVC" --type='json' -p '[{"op":"replace","path":"/spec/type","value":"ClusterIP"},{"op":"remove","path":"/spec/ports/0/nodePort"},{"op":"remove","path":"/spec/ports/1/nodePort"},{"op":"remove","path":"/spec/ports/2/nodePort"},{"op":"remove","path":"/spec/ports/3/nodePort"}]' 
-else
-   kubectl -n "$LOG_NS" patch svc "$SVC" --type='json' -p '[{"op":"replace","path":"/spec/type","value":"ClusterIP"}]'
-fi
-
+kubectl -n "$LOG_NS" patch svc "$SVC" --type='json' -p '[{"op":"replace","path":"/spec/type","value":"ClusterIP"}]'
