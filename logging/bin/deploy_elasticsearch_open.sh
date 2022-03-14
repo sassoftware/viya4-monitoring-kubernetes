2f4f447e97f462d63a5b0dbe0f7353b50e6c0816
cumcke <90708015+cumcke@users.noreply.github.com>
2022-03-14 15:40:32 -0400

    Updating sleeps to conditional until loops in Elasticsearch deployment (#313)
    * Removing unused variable that was causing errors in deployment
    
    * Changing generic waits to check loops.
    
    * Changed generic waits to until loops to improve performance.
    
    * Updating until loop conditions to avoid errors.
    
    * More fixes to conditional logic
    
    * Removing debug messages
    
    * Adding variable name so the loop works.

--- logging/bin/deploy_elasticsearch_open.sh
+++ logging/bin/deploy_elasticsearch_open.sh
@@ -317,12 +317,18 @@ else
    log_debug "**********************************>Multirole Flag: $ES_MULTIROLE_NODES"
 fi
 
-# wait for pod to come up
-log_verbose "Waiting [90] seconds to allow PVCs for pod [v4m-es-master-0] to be matched with available PVs [$(date)]"
-sleep 90
+# waiting for PVCs to be bound
+declare -i pvcCounter=0
+pvc_status=$(kubectl -n $LOG_NS get pvc  data-v4m-es-master-0  -o=jsonpath="{.status.phase}")
+until [ $pvc_status=="Bound" ] || (( $pvcCounter>90 )); 
+do 
+   sleep 5
+   pvcCounter=$((pvcCounter+5))
+   pvc_status=$(kubectl -n $LOG_NS get pvc data-v4m-es-master-0 -o=jsonpath="{.status.phase}")
+done
 
 # Confirm PVC is "bound" (matched) to PV
-pvc_status=$(kubectl -n $LOG_NS get pvc  data-v4m-es-master-0  -o=jsonpath="{.status.phase}")
+
 if [ "$pvc_status" != "Bound" ];  then
       log_error "It appears that the PVC [data-v4m-es-master-0] associated with the [v4m-es-master-0] node has not been bound to a PV."
       log_error "The status of the PVC is [$pvc_status]"
@@ -338,8 +344,21 @@ kubectl -n $LOG_NS wait pods v4m-es-master-0 --for=condition=Ready --timeout=10m
 # hitting https:/host:port -u adminuser:adminpwd --insecure 
 # returns "Open Distro Security not initialized." and 503 when up
 
-log_verbose "Waiting [2] minutes to allow Elasticsearch to initialize [$(date)]"
-sleep 120
+set -e
+
+log_verbose "Waiting for Elasticsearch to initialize - timeout 3 min [$(date)]"
+declare -i esCounter=0
+es_status=$(echo $(kubectl logs -n $LOG_NS v4m-es-master-0 | grep -s "Node 'v4m-es-master-0' initialized"))
+until [ -n "$es_status" ] || (( $esCounter>180 ));
+do
+   sleep 20
+   esCounter=$((esCounter+20))
+   es_status=$(echo $(kubectl logs -n $LOG_NS v4m-es-master-0 | grep -s "Node 'v4m-es-master-0' initialized"))
+done
+
+if [ -z "$es_status" ];  then
+      log_warn "Unable to verify that the Elasticsearch pod [v4m-es-master-0] has initialized."
+fi
 
 set +e
 
