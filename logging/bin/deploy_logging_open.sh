d9f2c96742a13a8994f4b0f9e8717c9b74199c34
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2022-05-23 13:09:27 -0400

    Added 'stubs' for obsolete deployment/removal scripts

---
+++ logging/bin/deploy_logging_open.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Copyright Â© 2022, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+cd "$(dirname $BASH_SOURCE)/../.."
+source logging/bin/common.sh
+
+log_error "This script is obsolete."
+log_error "Run logging/bin/deploy_logging.sh instead."
+
