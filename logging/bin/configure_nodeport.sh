bfd141ed54d496f14c03ba38c0a644311545a072
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2022-06-20 13:01:59 -0400

    [CHANGE] replace es_nodeport_enable/disable script w/configure_nodeport.sh script

--- logging/bin/configure_nodeport.sh
+++ logging/bin/configure_nodeport.sh
@@ -6,6 +6,10 @@
 cd "$(dirname $BASH_SOURCE)/../.."
 source logging/bin/common.sh
 
+#TO DO: Should be done in bin/common?
+export LOG_NS="${LOG_NS:-logging}"
+export MON_NS="${MON_NS:-monitoring}"
+
 # Confirm NOT on OpenShift
 if [ "$OPENSHIFT_CLUSTER" == "true" ]; then
   if [ "${CHECK_OPENSHIFT_CLUSTER:-true}" == "true" ]; then
@@ -17,30 +21,67 @@ fi
 
 set -e
 
-namespace="$LOG_NS"
-
 app=${1}
 arg2=${2}
+arg2=$(echo "$arg2"| tr '[:lower:]' '[:upper:]')
 
 if [[ "$arg2" -ge 30000 ]] && [[ "$arg2" -le 32767 ]]; then
+   log_debug "Requested port [$target_port] is within valid range"
    target_port="$arg2"
-elif [ "$arg2" == "DISABLE" ] || [ "$arg2" == "0" ] || [ -z "$arg2" ]; then
+elif [ "$arg2" == "0" ]; then
+   log_debug "Random port requested"
+   target_port="0"
+elif [ "$arg2" == "DISABLE" ] || [ -z "$arg2" ]; then
    log_debug "2nd argument: $arg2"
 else
-   log_error "Invalid port number provided."
-   log_error "Value must be: a number between 30000 and 32767,  0 (a random port will be assigned) or DISABLE (disable the current nodePort). "
+   log_error "Invalid 2nd argument provided."
+   log_error "Value must be: a port number between 30000 and 32767,  0 (a random port will be assigned) or DISABLE (disable the current nodePort). "
    exit 1
 fi
 
 app=$(echo "$app"| tr '[:lower:]' '[:upper:]')
 case "$app" in
-   "OPENSEARCH"|"OS"|"ELASTICSEARCH"|"ES")
-      SVC=$ES_SERVICENAME
+   "OPENSEARCH"|"OS")
+      namespace=$LOG_NS
+      servicename=$ES_SERVICENAME
+      appname="OPENSEARCH"
       target_port="${target_port:-0}"
       ;;
-   "OPENSEARCHDASHBOARDS"|"DASHBOARDS"|"OSD"|"KIBANA"|"KB")
-      SVC=$KB_SERVICENAME
+   "ELASTICSEARCH"|"ES")
+      namespace=$LOG_NS
+      servicename=$ES_SERVICENAME
+      appname="ELASTICSEARCH"
+      target_port="${target_port:-0}"
+      ;;
+   "OPENSEARCHDASHBOARDS"|"DASHBOARDS"|"OSD")
+      namespace=$LOG_NS
+      servicename=$KB_SERVICENAME
+      appname="OPENSEARCHDASHBOARDS"
       target_port="${target_port:-31033}"
+      ;;
+   "KIBANA"|"KB")
+      namespace=$LOG_NS
+      servicename=$KB_SERVICENAME
+      appname="KIBANA"
+      target_port="${target_port:-31033}"
+      ;;
+   "ALERTMANAGER"|"AM")
+      namespace=$MON_NS
+      appname="ALERTMANAGER"
+      servicename="v4m-alertmanager"
+      target_port="${target_port:-31091}"
+      ;;
+   "PROMETHEUS"|"PROM"|"PRO"|"PR")
+      namespace=$MON_NS
+      appname="PROMETHEUS"
+      servicename="v4m-prometheus"
+      target_port="${target_port:-31090}"
+      ;;
+   "GRAFANA"|"GRAF"|"GR")
+      namespace=$MON_NS
+      appname="GRAFANA"
+      servicename="v4m-grafana"
+      target_port="${target_port:-31100}"
       ;;
   ""|*)
       log_error "Application name is invalid or missing."
@@ -50,20 +91,20 @@ case "$app" in
 esac
 
 if [ "$arg2" == "DISABLE" ];then
-   log_info "Removing NodePort for [$SVC] in [$LOG_NS]"
-   kubectl -n "$LOG_NS" patch svc "$SVC" --type='json' -p '[{"op":"replace","path":"/spec/type","value":"ClusterIP"}]'
+   log_info "Removing NodePort for [$servicename] in [$LOG_NS]"
+   kubectl -n "$namespace" patch svc "$servicename" --type='json' -p '[{"op":"replace","path":"/spec/type","value":"ClusterIP"}]'
    exit
 elif [ "$target_port" != "0" ]; then
-   log_info "Making [$SVC] in [$LOG_NS] namespace available on port [$target_port]"
+   log_info "Making [$servicename] in [$namespace] namespace available on port [$target_port]"
    echo
 else
-   log_debug "No specific port was provided, will make [$SVC] available on a random port"
+   log_debug "No specific port was provided, will make [$servicename] available on a random port"
 fi
 
-kubectl -n "$LOG_NS" patch svc "$SVC" --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]'
-kubectl -n "$LOG_NS" patch svc "$SVC" --type='json' -p '[{"op":"replace","path":"/spec/ports/0/nodePort","value":'${target_port}'}]'
+kubectl -n "$namespace" patch svc "$servicename" --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]'
+kubectl -n "$namespace" patch svc "$servicename" --type='json' -p '[{"op":"replace","path":"/spec/ports/0/nodePort","value":'${target_port}'}]'
 
-SHOW_ES_URL="${SHOW_ES_URL:-true}"
-if [ "$SHOW_ES_URL" == "true" ]; then
-   bin/show_app_url.sh $app
+show_url="${SHOW_ES_URL:-true}"
+if [ "$show_url" == "true" ]; then
+   bin/show_app_url.sh $appname
 fi
