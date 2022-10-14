4748834565bcab9e67890e7add75582f8d8bff94
Greg Smith <65406958+gsmith-sas@users.noreply.github.com>
2022-10-14 14:56:16 -0400

    Roll back to older OSD Helm chart (#415)

--- logging/bin/deploy_osd.sh
+++ logging/bin/deploy_osd.sh
@@ -37,7 +37,11 @@ if [ "$(kubectl get ns $LOG_NS -o name 2>/dev/null)" == "" ]; then
 fi
 
 # Get/Set Helm Chart Version
-OSD_HELM_CHART_VERSION=${OSD_HELM_CHART_VERSION:-"1.8.3"}
+#12OCT22: Reverting back to OSD Helm Chart version 1.5.1 
+#         due to problem with Helm chart ver 1.8.3 which
+#         'matches' OSD version 1.3.5.  Problem involves
+#         ingress.path.backend.serverName resolution.
+OSD_HELM_CHART_VERSION=${OSD_HELM_CHART_VERSION:-"1.5.1"}
 
 # get credentials
 export ES_KIBANASERVER_PASSWD=${ES_KIBANASERVER_PASSWD}
