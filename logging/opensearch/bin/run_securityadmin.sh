d6f7a82c7776e5ef66e9e7375988b12932baee47
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2023-01-19 20:46:24 -0500

    Cleanup

--- logging/opensearch/bin/run_securityadmin.sh
+++ logging/opensearch/bin/run_securityadmin.sh
@@ -10,7 +10,6 @@ export ES_CONF_DIR=/usr/share/opensearch/config
 logfile=$ES_CONF_DIR/run_securityadmin.log
 echo "run_securityadmin.sh script starting [$(date)]" > $logfile
 
-#if [[ ! -x $ES_PLUGINS_DIR/opensearch-security/tools/securityadmin.sh ]]; then
 if [ ! -x $ES_PLUGINS_DIR/opensearch-security/tools/securityadmin.sh ]; then
    echo "Setting eXecutable bit"
   chmod +x $ES_PLUGINS_DIR/opensearch-security/tools/securityadmin.sh
