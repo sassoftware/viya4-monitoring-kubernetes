c6283b969b3bc6886e228214ad2adfb505c02e04
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2023-08-22 16:50:50 -0400

    Update comment re:currently supported SAS Viya releases

--- bin/kube-include.sh
+++ bin/kube-include.sh
@@ -25,13 +25,13 @@ fi
 
 # SAS Viya 4 versions
 # supported by SAS Tech Support
-# Updated: 24JUL23
+# Updated: 22AUG23
 # 2022.09 LTS 1.21 1.24
 # 2023.03 LTS 1.23 1.25
-# 2023.04     1.23 1.25
 # 2023.05     1.24 1.26
 # 2023.06     1.24 1.26
 # 2023.07     1.24 1.26
+# 2023.08     1.24 1.26
 if [[ $KUBE_SERVER_VER =~ v1.2[1-9] ]]; then
   :
 else 
