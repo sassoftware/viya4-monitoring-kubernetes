ff23210f92cb97ea2204e37f5633e340687cea31
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2023-10-19 11:40:33 -0400

    Update K8s/OpenShift version checking logic

--- bin/kube-include.sh
+++ bin/kube-include.sh
@@ -15,8 +15,7 @@ KUBE_CLIENT_VER=$(kubectl version --short | grep 'Client Version' | awk '{print 
 KUBE_SERVER_VER=$(kubectl version --short | grep 'Server Version' | awk '{print $3}' 2>/dev/null)
 
 # Client version allowed to be one minor version earlier than minimum server version
-# 23MAR23: In spite of above, limiting kubectl to 1.2.x for now
-if [[ $KUBE_CLIENT_VER =~ v1.2[0-9] ]]; then
+if [[ $KUBE_CLIENT_VER =~ v1.2[2-9] ]]; then
   :
 else 
   log_warn "Unsupported kubectl version: [$KUBE_CLIENT_VER]."
@@ -25,14 +24,14 @@ fi
 
 # SAS Viya 4 versions
 # supported by SAS Tech Support
-# Updated: 25SEP23
-# 2022.09 LTS 1.21 1.24
+# Updated: 18OCT23
 # 2023.03 LTS 1.23 1.25
-# 2023.06     1.24 1.26
+# 2023.10 LTS 1.24 1.27
 # 2023.07     1.24 1.26
 # 2023.08     1.24 1.26
 # 2023.09     1.25 1.27
-if [[ $KUBE_SERVER_VER =~ v1.2[1-9] ]]; then
+# 2023.10     1.25 1.27
+if [[ $KUBE_SERVER_VER =~ v1.2[3-9] ]]; then
   :
 else 
   log_warn "The detected version of Kubernetes [$KUBE_SERVER_VER] is not supported by any of the currently supported releases of SAS Viya."
