ddfef8b391f0181274acb5dd70faa18fd4ce0489
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2024-06-04 14:21:21 -0400

    Housekeeping

--- bin/kube-include.sh
+++ bin/kube-include.sh
@@ -24,14 +24,15 @@ fi
 
 # SAS Viya 4 versions
 # supported by SAS Tech Support
-# Updated: 17APR24
+# Updated: 30MAY24
 # 2022.09 LTS 1.21 1.24
 # 2023.03 LTS 1.23 1.25
 # 2023.10 LTS 1.25 1.27
-# 2024.01     1.25 1.27
+# 2024.03 LTS 1.26 1.28
 # 2024.02     1.26 1.28
 # 2024.03     1.26 1.28
 # 2024.04     1.26 1.28
+# 2024.05     1.27 1.29
 
 if [[ $KUBE_SERVER_VER =~ v1.2[1-9] ]]; then
   :
