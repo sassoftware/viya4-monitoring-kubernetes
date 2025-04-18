1c8069f8248c1e2907659d5a669c4c2ab266a9b0
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2025-04-18 11:12:18 -0400

    Adjust indents to match standard

--- bin/kube-include.sh
+++ bin/kube-include.sh
@@ -7,29 +7,29 @@
 # Assumes bin/common.sh has been sourced
 
 if [ ! "$(which kubectl)" ]; then
-  log_error "kubectl not found on the current PATH"
-  exit 1
+    log_error "kubectl not found on the current PATH"
+    exit 1
 fi
 
 KUBE_CLIENT_VER=$(kubectl version --output=json | tr -d '\n' | tr -s " " | sed -E 's/^\{.*"clientVersion": \{([^\}]+)}.*/\1\n/' | sed -E 's/.*"gitVersion": "([^\"]*)".*$/\1/')
 KUBE_SERVER_VER=$(kubectl version --output=json | tr -d '\n' | tr -s " " | sed -E 's/^\{.*"serverVersion": \{([^\}]+)}.*/\1\n/' | sed -E 's/.*"gitVersion": "([^\"]*)".*$/\1/')
 
 if [[ "$KUBE_CLIENT_VER" =~ v([0-9]+)\.([0-9]+)\.([0-9]+) ]]; then
-   KUBE_CLIENT_MAJOR=${BASH_REMATCH[1]}
-   KUBE_CLIENT_MINOR=${BASH_REMATCH[2]}
-   KUBE_CLIENT_PATCH=${BASH_REMATCH[3]}
-   log_debug "Kubernetes CLIENT - Major: [$KUBE_CLIENT_MAJOR] Minor: [$KUBE_CLIENT_MINOR] Patch: [$KUBE_CLIENT_PATCH]"
+    KUBE_CLIENT_MAJOR=${BASH_REMATCH[1]}
+    KUBE_CLIENT_MINOR=${BASH_REMATCH[2]}
+    KUBE_CLIENT_PATCH=${BASH_REMATCH[3]}
+    log_debug "Kubernetes CLIENT - Major: [$KUBE_CLIENT_MAJOR] Minor: [$KUBE_CLIENT_MINOR] Patch: [$KUBE_CLIENT_PATCH]"
 else
    log_error "Kubernetes Client Version does not match expected pattern.";
 fi;
 
 if [[ "$KUBE_SERVER_VER" =~ v([0-9]+)\.([0-9]+)\.([0-9]+) ]]; then
-   KUBE_SERVER_MAJOR=${BASH_REMATCH[1]}
-   KUBE_SERVER_MINOR=${BASH_REMATCH[2]}
-   KUBE_SERVER_PATCH=${BASH_REMATCH[3]}
-   log_debug "Kubernetes SERVER - Major: [$KUBE_SERVER_MAJOR] Minor: [$KUBE_SERVER_MINOR] Patch: [$KUBE_SERVER_PATCH]"
+    KUBE_SERVER_MAJOR=${BASH_REMATCH[1]}
+    KUBE_SERVER_MINOR=${BASH_REMATCH[2]}
+    KUBE_SERVER_PATCH=${BASH_REMATCH[3]}
+    log_debug "Kubernetes SERVER - Major: [$KUBE_SERVER_MAJOR] Minor: [$KUBE_SERVER_MINOR] Patch: [$KUBE_SERVER_PATCH]"
 else
-   log_error "Kubernetes SERVER Version does not match expected pattern.";
+    log_error "Kubernetes SERVER Version does not match expected pattern.";
 fi;
 
 # SAS Viya 4 versions
@@ -47,17 +47,17 @@ fi;
 
 # Client version allowed to be one minor version earlier than minimum server version
 if [ "$KUBE_CLIENT_MAJOR" -eq "1" ] && [ "$KUBE_CLIENT_MINOR" -gt "22" ]; then
-  :
+    :
 else
-  log_warn "Unsupported kubectl version: [$KUBE_CLIENT_VER]."
-  log_warn "This script might not work as expected. Support might not be available until kubectl is upgraded to a supported version."
+    log_warn "Unsupported kubectl version: [$KUBE_CLIENT_VER]."
+    log_warn "This script might not work as expected. Support might not be available until kubectl is upgraded to a supported version."
 fi
 
 if [ "$KUBE_SERVER_MAJOR" -eq "1" ] && [ "$KUBE_SERVER_MINOR" -gt "23"  ]; then
-  :
+    :
 else
-  log_warn "The detected version of Kubernetes [$KUBE_SERVER_VER] is not supported by any of the currently supported releases of SAS Viya."
-  log_warn "This script might not work as expected. Support might not be available until Kubernetes is upgraded to a supported version."
+    log_warn "The detected version of Kubernetes [$KUBE_SERVER_VER] is not supported by any of the currently supported releases of SAS Viya."
+    log_warn "This script might not work as expected. Support might not be available until Kubernetes is upgraded to a supported version."
 fi
 
 export KUBE_CLIENT_VER="$KUBE_CLIENT_VER"
