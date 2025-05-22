b0a308bc17be09e21661590479efe070338ca4c7
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2025-05-22 18:10:22 -0400

    Linting: deploy_openshift_prereqs.sh

--- logging/bin/deploy_openshift_prereqs.sh
+++ logging/bin/deploy_openshift_prereqs.sh
@@ -3,10 +3,10 @@
 # Copyright © 2021, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-cd "$(dirname $BASH_SOURCE)/../.."
+cd "$(dirname "$BASH_SOURCE")/../.." || exit 1
 source logging/bin/common.sh
 
-this_script=`basename "$0"`
+this_script=$(basename "$0")
 
 log_debug "Script [$this_script] has started [$(date)]"
 
@@ -14,26 +14,26 @@ log_debug "Script [$this_script] has started [$(date)]"
 OPENSHIFT_PREREQS_ENABLE=${OPENSHIFT_PREREQS_ENABLE:-true}
 
 if [ "$OPENSHIFT_PREREQS_ENABLE" != "true" ]; then
-  log_info "Environment variable [OPENSHIFT_PREREQS_ENABLE] is not set to 'true'; exiting WITHOUT deploying OpenShift Prerequisites"
-  exit
+    log_info "Environment variable [OPENSHIFT_PREREQS_ENABLE] is not set to 'true'; exiting WITHOUT deploying OpenShift Prerequisites"
+    exit
 fi
 
 
 # link OpenSearch serviceAccounts to 'privileged' scc
-oc adm policy add-scc-to-user privileged -z v4m-os -n $LOG_NS
+oc adm policy add-scc-to-user privileged -z v4m-os -n "$LOG_NS"
 
 # create the 'v4m-logging-v2' SCC, if it does not already exist
-if oc get scc v4m-logging-v2 2>/dev/null 1>&2; then
-   log_info "Skipping scc creation; using existing scc [v4m-logging-v2]"
+if oc get scc v4m-logging-v2 2> /dev/null 1> &2; then
+    log_info "Skipping scc creation; using existing scc [v4m-logging-v2]"
 else
-   oc create -f logging/openshift/fb_v4m-logging-v2_scc.yaml
+    oc create -f logging/openshift/fb_v4m-logging-v2_scc.yaml
 fi
 
 # create the 'v4m-k8sevents' SCC, if it does not already exist
-if oc get scc v4m-k8sevents 2>/dev/null 1>&2; then
-   log_info "Skipping scc creation; using existing scc [v4m-k8sevents]"
+if oc get scc v4m-k8sevents 2> /dev/null 1> &2; then
+    log_info "Skipping scc creation; using existing scc [v4m-k8sevents]"
 else
-   oc create -f logging/openshift/fb_v4m-k8sevents_scc.yaml
+     oc create -f logging/openshift/fb_v4m-k8sevents_scc.yaml
 fi
 
 log_info "OpenShift Prerequisites have been deployed."
