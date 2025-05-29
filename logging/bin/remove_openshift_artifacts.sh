26dc010738cb3c72218e341ad340bfb06f3d449f
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2025-05-29 16:57:51 -0400

    Linting: remove_openshift_artifacts.sh

--- logging/bin/remove_openshift_artifacts.sh
+++ logging/bin/remove_openshift_artifacts.sh
@@ -3,10 +3,10 @@
 # Copyright © 2021, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-cd "$(dirname $BASH_SOURCE)/../.."
+cd "$(dirname "$BASH_SOURCE")/../.." || exit 1
 source logging/bin/common.sh
 
-this_script=`basename "$0"`
+this_script=$(basename "$0")
 
 log_debug "Script [$this_script] has started [$(date)]"
 
@@ -14,16 +14,14 @@ log_debug "Script [$this_script] has started [$(date)]"
 OPENSHIFT_ARTIFACTS_REMOVE=${OPENSHIFT_ARTIFACTS_REMOVE:-true}
 
 if [ "$OPENSHIFT_ARTIFACTS_REMOVE" != "true" ]; then
-  log_info "Environment variable [OPENSHIFT_ARTIFACTS_REMOVE] is not set to 'true'; exiting WITHOUT removing OpenShift Artifacts"
-  exit
+    log_info "Environment variable [OPENSHIFT_ARTIFACTS_REMOVE] is not set to 'true'; exiting WITHOUT removing OpenShift Artifacts"
+    exit
 fi
 
 # remove custom OpenShift SCC
-oc delete scc v4mlogging     --ignore-not-found
+oc delete scc v4mlogging --ignore-not-found
 oc delete scc v4m-logging-v2 --ignore-not-found
-oc delete scc v4m-k8sevents  --ignore-not-found
-
-
+oc delete scc v4m-k8sevents --ignore-not-found
 
 log_info "OpenShift Prerequisites have been removed."
 
