742e989831b3f228b3489786a16059dc4938f791
gsmith-sas <65406958+gsmith-sas@users.noreply.github.com>
2025-05-30 16:41:57 -0400

    Linting: remove_openshift_routes.sh

--- logging/bin/remove_openshift_routes.sh
+++ logging/bin/remove_openshift_routes.sh
@@ -3,17 +3,17 @@
 # Copyright © 2021, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-cd "$(dirname $BASH_SOURCE)/../.."
+cd "$(dirname "$BASH_SOURCE")/../.." || exit 1
 source logging/bin/common.sh
 
-this_script=`basename "$0"`
+this_script=$(basename "$0")
 
 log_debug "Script [$this_script] has started [$(date)]"
 
-oc -n $LOG_NS delete route v4m-es-kibana-svc     --ignore-not-found
-oc -n $LOG_NS delete route v4m-es-client-service --ignore-not-found
-oc -n $LOG_NS delete route v4m-search            --ignore-not-found
-oc -n $LOG_NS delete route v4m-osd               --ignore-not-found
+oc -n "$LOG_NS" delete route v4m-es-kibana-svc --ignore-not-found
+oc -n "$LOG_NS" delete route v4m-es-client-service --ignore-not-found
+oc -n "$LOG_NS" delete route v4m-search --ignore-not-found
+oc -n "$LOG_NS" delete route v4m-osd --ignore-not-found
 
 log_info "OpenShift Routes have been removed."
 
