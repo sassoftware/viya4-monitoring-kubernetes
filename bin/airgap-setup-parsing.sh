1c56b9f2318f4e78f2fb516301f7ec793786791c
dasali <David.Salinas@sas.com>
2025-12-22 15:40:49 -0500

    fix: more formatting changes and changing redundant code (COMPOBSERVE-912)

--- bin/airgap-setup-parsing.sh
+++ bin/airgap-setup-parsing.sh
@@ -20,26 +20,43 @@ done
 if [[ -n $AIRGAP_HELM_REPO && $AIRGAP_HELM_REPO != "$AIRGAP_REGISTRY" ]]; then
     log_warn "AIRGAP_HELM_REPO ($AIRGAP_HELM_REPO) does not match AIRGAP_REGISTRY ($AIRGAP_REGISTRY)."
     log_warn "This script currently does not support the storing of two types of different artifacts in different locations."
-    echo
 fi
+
+get_chart_info() {
+    local chart_name_var="$1"
+
+    prefix=${chart_name_var%_CHART_NAME}
+    chart_name="${!chart_name_var}"
+    repo_name_var="${prefix}_CHART_REPO"
+    version_var="${prefix}_CHART_VERSION"
+
+    repo_name="${!repo_name_var:-}"
+    version="${!version_var:-}"
+
+    if [[ -z $repo_name || -z $chart_name || -z $version ]]; then
+        log_error "Missing at least one of the following values:"
+        log_error "Repo's Name: ${repo_name}"
+        log_error "Chart's Name: ${chart_name}"
+        log_error "Version number: ${version}"
+        exit 1
+    fi
+}
 
 log_info "docker login ""$AIRGAP_REGISTRY"" -u ""$AIRGAP_REGISTRY_USERNAME"" -p ___"
 docker login "$AIRGAP_REGISTRY" -u "$AIRGAP_REGISTRY_USERNAME" -p "$AIRGAP_REGISTRY_PASSWORD"
 
-log_info "Parsing $COMPONENT_FILE"
-echo
-
 log_notice "Step 1 - Import Images"
-echo
 
-for var in $(compgen -A variable | grep '_FULL_IMAGE$'); do
+while IFS='=' read -r var _; do
     full_image="${!var}"
 
-    [[ -z $full_image ]] && continue
+    if [[ -z $full_image ]]; then
+        log_error "Failure when getting full image value: ${full_image}"
+        exit 1
+    fi
 
     if [[ $full_image == "registry.redhat.io/openshift4/ose-oauth-proxy:latest" ]]; then
         log_warn "Skipping image: $full_image"
-        echo
         continue
     fi
 
@@ -50,18 +67,13 @@ for var in $(compgen -A variable | grep '_FULL_IMAGE$'); do
 
     log_verbose "docker pull ""${full_image}"""
     docker pull "${full_image}"
-    echo
     log_verbose "docker tag ""${full_image}"" ""$AIRGAP_REGISTRY""/""${repo_image}"""
     docker tag "${full_image}" "$AIRGAP_REGISTRY"/"${repo_image}"
-    echo
     log_verbose "docker push ""$AIRGAP_REGISTRY""/""${repo_image}"""
     docker push "$AIRGAP_REGISTRY"/"${repo_image}"
-    echo
-done
-echo
+done < <(env | grep '_FULL_IMAGE=')
 
 log_notice "Step 2 - Helm Repo Add Commands"
-echo
 
 declare -A REPO_URLS=(
     ["prometheus-community"]="https://prometheus-community.github.io/helm-charts"
@@ -70,74 +82,48 @@ declare -A REPO_URLS=(
     ["opensearch"]="https://opensearch-project.github.io/helm-charts"
 )
 
-for var in $(compgen -A variable | grep '_CHART_REPO'); do
+while IFS='=' read -r var _; do
     repo_name="${!var}"
     repo_url=${REPO_URLS[$repo_name]:-}
 
     if [[ -z $repo_url ]]; then
-        log_warn "No URL for repo '$repo_name'. Skipping."
-        echo
-        continue
+        log_error "No URL for repo '$repo_name'"
+        exit 1
     fi
 
     log_info "helm repo add ""${repo_name}"" ""${repo_url}"""
     helm repo add "${repo_name}" "${repo_url}"
-done
-echo
+done < <(env | grep '_CHART_REPO=')
 
 log_notice "Step 3 - Helm Pull Commands"
-echo
 
 log_info "helm repo update"
 helm repo update
-echo
 
-for var in $(compgen -A variable | grep '_CHART_NAME'); do
-    prefix=${var%_CHART_NAME}
-    chart_name="${!var}"
-    repo_name_var="${prefix}_CHART_REPO"
-    version_var="${prefix}_CHART_VERSION"
-
-    repo_name="${!repo_name_var:-}"
-    version="${!version_var:-}"
-
-    [[ -z $repo_name || -z $chart_name || -z $version ]] && continue
+while IFS='=' read -r var _; do
+    get_chart_info "$var"
 
     log_verbose "helm pull --destination ""$TMP_DIR"" --version ""${version}"" ""${repo_name}""/""${chart_name}"""
     helm pull --destination "$TMP_DIR" --version "${version}" "${repo_name}"/"${chart_name}"
-done
-echo
+done < <(env | grep '_CHART_NAME=')
 
 log_notice "Step 4 - Helm Chart Push"
-echo
 
-log_info "helm registry login ""$AIRGAP_REGISTRY"" -u ""$AIRGAP_REGISTRY_USERNAME"" -p ___"
+log_info "Logging into the private registry, ""$AIRGAP_REGISTRY"", with ""$AIRGAP_REGISTRY_USERNAME"" as the username"
 helm registry login "$AIRGAP_REGISTRY" -u "$AIRGAP_REGISTRY_USERNAME" -p "$AIRGAP_REGISTRY_PASSWORD"
-echo
 
-for archive_path in "$TMP_DIR"/*.tgz; do
-    [[ -f $archive_path ]] || continue
+while IFS='=' read -r var _; do
+    get_chart_info "$var"
 
-    archive_name=$(basename "$archive_path")
+    archive_path="${TMP_DIR}/${chart_name}-${version}.tgz"
 
-    if [[ $archive_name == *prometheus* ]]; then
-        repo_name="prometheus-community"
-    elif [[ $archive_name == *fluent* ]]; then
-        repo_name="fluent"
-    elif [[ $archive_name == *grafana* || $archive_name == *tempo* ]]; then
-        repo_name="grafana"
-    elif [[ $archive_name == *opensearch* ]]; then
-        repo_name="opensearch"
-    else
-        log_error "Unknown helm repo for archive: ${archive_name}"
+    if [[ -z $archive_path ]]; then
+        log_error "Chart archive not found: ${archive_path}"
         exit 1
     fi
 
     log_info "helm push \"${archive_path}\" oci://$AIRGAP_REGISTRY/${repo_name}"
     helm push "${archive_path}" "oci://$AIRGAP_REGISTRY/${repo_name}"
-done
-echo
+done < <(env | grep '_CHART_NAME=')
 
-echo
 log_notice "Script Complete."
-echo
