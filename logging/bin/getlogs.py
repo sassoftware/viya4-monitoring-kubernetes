3ce181008025e90cef500bb08bc9c9cadc8d1b2e
VISHAAN SAXENA <Vishaan.Saxena@sas.com>
2026-02-19 19:58:44 -0500

    Changelog + Copyright

--- logging/bin/getlogs.py
+++ logging/bin/getlogs.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 
-# Copyright Â© 2023, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+# Copyright Â© 2023-2026, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 import sys
 
